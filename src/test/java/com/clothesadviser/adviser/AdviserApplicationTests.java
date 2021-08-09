5acb71f3f0705cea4a07f633ddc492539f06be6f
Dilion Mihail <dilionmihai@gmail.com>
2021-08-09 21:23:19 +0300

    Initial commit

---
+++ src/test/java/com/clothesadviser/adviser/AdviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothesadviser.adviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
